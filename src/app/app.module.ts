99a80d98d73665b254bba9ee4df9491d560f2daf
medb2m <79202818+medb2m@users.noreply.github.com>
2024-04-23 22:32:26 +0100

    Created the resume Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { ProjectModalComponent } from './project-modal/project-modal.component'
 import { CarouselModule } from 'ngx-bootstrap/carousel'
 import { CollapseModule } from 'ngx-bootstrap/collapse'
+import { AccordionModule } from 'ngx-bootstrap/accordion'
 import { FormsModule } from '@angular/forms';
 
 
@@ -37,6 +38,7 @@ import { FormsModule } from '@angular/forms';
     ModalModule.forRoot(),
     CarouselModule.forRoot(),
     CollapseModule.forRoot(),
+    AccordionModule.forRoot(),
     FormsModule
   ],
   providers: [],
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,9 +8,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class ResumeComponent {
 
+  isWorkExperienceOpen : boolean = false
+  isEducationOpen : boolean = false
+  isCertificateOpen : boolean = false
+  isSkillsOpen : boolean = false
+
   constructor(
-    private titleService : Title
+    private titleService : Title,
+    private renderer : Renderer2
   ){
     this.titleService.setTitle('UVM - Resume')
+  }
+  DownloadFile() {
+    const link = this.renderer.createElement('a')
+    link.setAttribute('target','_blank')
+    link.setAttribute('href','../../assets/b2ms_Dev_Resume_2024.pdf')
+    link.setAttribute('download','b2ms_Dev_Resume_2024.pdf')
+    link.click()
+    link.remove()
   }
 }
